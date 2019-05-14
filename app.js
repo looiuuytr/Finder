750dcba14c78f7d107722b5c15fbf9a79b642a42
Louis DLH <sizeotomai@msn.com>
2019-05-14 20:53:10 +0200

    First research

--- app.js
+++ app.js
@@ -7,7 +7,45 @@ const router = express.Router();
 
 router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
-    //__dirname : It will resolve to your project folder.
+});
+
+router.get('/musics', function(req, res) {
+    res.sendFile(path.join(__dirname + '/musics.html'));
+});
+
+router.get('/search', function(req, res) {
+
+    //res.sendFile(path.join(__dirname + '/musics.html'));
+
+
+    const apiRouteUrl = "http://api.musixmatch.com/ws/1.1/track.search?q_lyrics=";
+    const params = "&page_size=10&page=1&s_track_rating=desc&apikey=5165cb025de6c85ef26ca5a8019c3c4e";
+
+    var tracks;
+
+    http.get(apiRouteUrl + req.query.searchBar + params, (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            tracks = JSON.parse(data).message.body.track_list;
+            console.log(tracks);
+            res.send(tracks);
+
+
+        });
+
+    }).on("error", (err) => {
+        //console.log("Error: " + err.message);
+    });
+
+
+
 });
 
 app.use('/', router);
