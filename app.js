4d9b3e9e8d3c49352210c2f55dda3ffc0d6a2c8c
Louis DLH <sizeotomai@msn.com>
2019-05-15 10:40:39 +0200

    Fixing var, const and let

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const express = require('express');
-var app = express();
+const app = express();
 const path = require('path');
-var SpotifyWebApi = require('spotify-web-api-node');
+const SpotifyWebApi = require('spotify-web-api-node');
 const router = express.Router();
 
-var spotifyApi = new SpotifyWebApi({
-    accessToken: 'BQBPIUa49adgLRFHSUUTxBLox0TSkQLTUenTkmMb77JBqbsdtih_DD6JrhhtRf8dYdp0pn0s8XB6qN7jNxWvyS-IlN9zEgU8VPaRuDvJOb5RQGcufW-SbHxz_eULUeCD5InfqiWkKcDS-dcdAoGYymTqmJPDaSPC-vm8Ot6nehptIAU',
+const spotifyApi = new SpotifyWebApi({
+    accessToken: 'BQDmgWuDRx6dfR4c0LKWJP_DsXMmA_iMdYdL1oA_AtgGWDT8pvUJdXZBfYSR4JDAJ-tO-l3lEaj-wCxwVFbndj49MictbxaLRaXDnAIw34d-CyoUJyOUXxWEW8Od_46bypj73KNJi_tVzmOlUufZUccviuvZ5M27SGpqdpDHpAjVlho',
     clientId: '515a6059023f42dabdc11f7722a731e3',
     clientSecret: '9b362d17ab8e49e1bba81eec59e4fe9b',
 });
@@ -22,13 +22,12 @@ router.get('/musics', function(req, res) {
 
 router.get('/search', function(req, res) {
 
-    //res.sendFile(path.join(__dirname + '/musics.html'));
 
 
     const apiRouteUrl = "http://api.musixmatch.com/ws/1.1/track.search?q_lyrics=";
     const params = "&page_size=20&page=1&s_track_rating=desc&apikey=5165cb025de6c85ef26ca5a8019c3c4e";
 
-    var tracks;
+    let tracks;
 
     http.get(apiRouteUrl + req.query.searchBar + params, (resp) => {
         let data = '';
@@ -41,7 +40,6 @@ router.get('/search', function(req, res) {
         // The whole response has been received. Print out the result.
         resp.on('end', () => {
             tracks = JSON.parse(data).message.body.track_list;
-            //console.log(tracks);
             res.send(tracks);
 
 
