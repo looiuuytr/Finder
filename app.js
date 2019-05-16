ca25ac18af98a3b3d07ddd56bd21dda06ac42d7f
Louis DLH <sizeotomai@msn.com>
2019-05-16 17:02:51 +0200

    Spotify token auth

--- app.js
+++ app.js
@@ -6,26 +6,58 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const router = express.Router();
 
 const spotifyApi = new SpotifyWebApi({
-    accessToken: 'BQDmgWuDRx6dfR4c0LKWJP_DsXMmA_iMdYdL1oA_AtgGWDT8pvUJdXZBfYSR4JDAJ-tO-l3lEaj-wCxwVFbndj49MictbxaLRaXDnAIw34d-CyoUJyOUXxWEW8Od_46bypj73KNJi_tVzmOlUufZUccviuvZ5M27SGpqdpDHpAjVlho',
     clientId: '515a6059023f42dabdc11f7722a731e3',
     clientSecret: '9b362d17ab8e49e1bba81eec59e4fe9b',
+    redirectUri: 'http://localhost:3000/callback'
 });
 
+var scopes = ['user-top-read'];
+var showDialog = true;
+var authorizeURL = spotifyApi.createAuthorizeURL(scopes, null, showDialog);
+
+app.get("/callback", async function (request, res) {
+    var authorizationCode = request.query.code;
+    const data = await spotifyApi.authorizationCodeGrant(authorizationCode);
+
+    try{
+        spotifyApi.setAccessToken(data.body.access_token);
+        res.redirect('/home');
+        }catch(err){
+            console.log('Something went wrong when retrieving the access token!', err.message);
+        }
+    
+        return;
+
+
+  });
+  
 
 router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    if (spotifyApi.getAccessToken() === undefined) {
+        res.redirect(authorizeURL);
+    } else {
+        res.redirect("/home");
+    }
+
 });
 
+router.get('/home', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 router.get('/musics', function(req, res) {
-    res.sendFile(path.join(__dirname + '/musics.html'));
+    if (spotifyApi.getAccessToken() === undefined) {
+        res.redirect(authorizeURL);
+    } else    res.sendFile(path.join(__dirname + '/musics.html'));
 });
 
 router.get('/search', function(req, res) {
 
-
+    if (spotifyApi.getAccessToken() === undefined) {
+        res.redirect(authorizeURL);
+    }
 
     const apiRouteUrl = "http://api.musixmatch.com/ws/1.1/track.search?q_lyrics=";
-    const params = "&page_size=20&page=1&s_track_rating=desc&apikey=5165cb025de6c85ef26ca5a8019c3c4e";
+    const params = "&page_size=50&page=1&s_track_rating=desc&apikey=5165cb025de6c85ef26ca5a8019c3c4e";
 
     let tracks;
 
@@ -53,24 +85,23 @@ router.get('/search', function(req, res) {
 
 });
 
-router.get('/play', function(req, res) {
+router.get('/play', async function(req, res) {
 
+    try{
     // Do search using the access token
-    spotifyApi.searchTracks('track:' + req.query.name + ' artist:' + req.query.artist, { limit: 1, offset: 1 }).then(
-        function(data) {
-            if (data.body.tracks.items[0]) {
-                res.send(data.body.tracks.items[0].uri);
-            } else {
-                res.send("");
-            }
+    const data = await spotifyApi.searchTracks('track:' + req.query.name + ' artist:' + req.query.artist, { limit: 1, offset: 1 });
 
-        },
-        function(err) {
-            console.log('Something went wrong!', err);
-        }
-    );
+    if (data.body.tracks.items[0]) {
+        res.send(data.body.tracks.items[0].uri);
+    } else {
+        res.send("");
+    }
+
+    }catch(err){
+        console.log(err)
+    }
+
     return;
-
 
 });
 
